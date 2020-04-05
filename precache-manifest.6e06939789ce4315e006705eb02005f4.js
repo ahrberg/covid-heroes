28cd9ec53c98252ef44411b95d4c60fbee603f75
ahrberg <ahrberg@kth.se>
2020-04-05 13:13:26 +0200

    Updates

--- precache-manifest.6e06939789ce4315e006705eb02005f4.js
+++ precache-manifest.6e06939789ce4315e006705eb02005f4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cafae8a85867d04e78f593adfcda5043",
+    "revision": "6bbad07b22fefc85df9e662b1778dc73",
     "url": "/covid-heroes/index.html"
   },
   {
-    "revision": "d00a057f8ebc59293755",
+    "revision": "1112f4aa9586e760cf19",
     "url": "/covid-heroes/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "ff2dd472a935383924c0",
-    "url": "/covid-heroes/static/js/2.2da00842.chunk.js"
+    "revision": "8b4501d81773f43a5ec0",
+    "url": "/covid-heroes/static/js/2.c1d16739.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/covid-heroes/static/js/2.2da00842.chunk.js.LICENSE.txt"
+    "url": "/covid-heroes/static/js/2.c1d16739.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d00a057f8ebc59293755",
-    "url": "/covid-heroes/static/js/main.4c8fc76b.chunk.js"
+    "revision": "1112f4aa9586e760cf19",
+    "url": "/covid-heroes/static/js/main.afae7468.chunk.js"
   },
   {
     "revision": "574ee0798ccd68f5b1d8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-heroes/precache-manifest.e8d1dd2c3770731a63bc77a25d069de7.js"
+  "/covid-heroes/precache-manifest.6e06939789ce4315e006705eb02005f4.js"
 );
 
 self.addEventListener('message', (event) => {
