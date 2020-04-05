a6ea540e499b78c76f3c8ba4b66d228590222292
ahrberg <ahrberg@kth.se>
2020-04-05 10:52:16 +0200

    Updates

---
+++ precache-manifest.cf0f8f16a69dfc802f1e1c9fd67669b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d363df72af6ed15bc6b3735e6e3920",
+    "url": "/covid-heroes/index.html"
+  },
+  {
+    "revision": "5d39fe94dc4ecc07294c",
+    "url": "/covid-heroes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff2dd472a935383924c0",
+    "url": "/covid-heroes/static/js/2.2da00842.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-heroes/static/js/2.2da00842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d39fe94dc4ecc07294c",
+    "url": "/covid-heroes/static/js/main.de93747d.chunk.js"
+  },
+  {
+    "revision": "574ee0798ccd68f5b1d8",
+    "url": "/covid-heroes/static/js/runtime-main.06c26233.js"
+  }
+]);
