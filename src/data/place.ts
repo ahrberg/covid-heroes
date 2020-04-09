0280f79f03a258717a848a0cafa8e6bcab544271
ahrberg <ahrberg@kth.se>
2020-04-09 10:51:46 +0200

    Changed hospital data

--- src/data/place.ts
+++ src/data/place.ts
@@ -8,37 +8,37 @@ export interface Place {
 export const places: Place[] = [
   {
     id: "45017923-68dc-4b08-8458-2c7254211ad7",
-    name: "Intensivvården Blekinge",
+    name: "Blekingesjukhuset",
     regionId: "1",
   },
   {
     id: "4738c3b7-da23-453b-86c9-8e73085244df",
-    name: "Intensivvården Dalarna",
+    name: "Mora lesarett",
     regionId: "2",
   },
   {
     id: "ce451927-7b23-4eab-b269-ade2c144e1b3",
-    name: "Näsa och hals Visby Lasarett",
+    name: "Visby Lasarett",
     regionId: "3",
   },
   {
     id: "ce451927-7b23-4eab-b269-ade2c144e3b3",
-    name: "Intensivvården SÖS",
+    name: "SÖS",
     regionId: "7",
   },
   {
     id: "ce451927-7b23-4eab-b269-ade2c144e3b3",
-    name: "Akuten Söderhamns sjukhus",
+    name: "Söderhamns sjukhus",
     regionId: "4",
   },
   {
     id: "ce451927-7b23-4eab-b269-ade2c144e3b3",
-    name: "Materialförådet på Hallands sjukhus",
+    name: "Hallands sjukhus",
     regionId: "5",
   },
   {
     id: "ce431927-7b23-4eab-b269-ade2c144e3b3",
-    name: "Röntgen på Östersunds sjukhus",
+    name: "Östersunds sjukhus",
     regionId: "6",
   },
 ];
